f26902dbc7eace58ffbf924c25872b7b6889d7c6
Sumanth <msumanth750@gmail.com>
2022-05-20 10:28:17 +0530

    black-dashboard add

--- ui/urls.py
+++ ui/urls.py
@@ -5,5 +5,14 @@ from ui import views
 urlpatterns = [
     path('',views.home,name='home'),
     path('index',views.index),
+    path('sales',views.sales),
+    path('expenses',views.expenses),
+    path('loans',views.loans),
+    path('brands',views.brands),
+    path('profile',views.profile),
+    path('staff',views.staff),
+    path('partners',views.partners),
+
+
     path('base/',views.viewbase)
 ]
--- ui/views.py
+++ ui/views.py
@@ -12,4 +12,19 @@ def viewbase(request):
     return render(request,'ui/base.html')
 
 def index(request):
-    return render(request,'ui/line.html')
+    lis=[50, 120, 105, 110, 95, 105, 90, 100, 80, 95, 70, 120]
+    return render(request,'blackdashboard/dashboard.html',{'lis':lis})
+def sales(request):
+    return render(request,'blackdashboard/sales.html')
+def expenses(request):
+    return render(request,'blackdashboard/expenses.html')
+def loans(request):
+    return render(request,'blackdashboard/loans.html')
+def brands(request):
+    return render(request,'blackdashboard/brands.html')
+def partners(request):
+    return render(request,'blackdashboard/partners.html')
+def staff(request):
+    return render(request,'blackdashboard/staff.html')
+def profile(request):
+    return render(request,'blackdashboard/profile.html')
